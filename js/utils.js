6c164ce5953483cdde992a01d2a6cc9f30e1dbf0
orgtarefas <orgtarefas.site@gmail.com>
2026-01-29 17:27:56 -0300

    Refactor utils.js and add global constants

--- js/utils.js
+++ js/utils.js
@@ -1,45 +1,25 @@
-// utils.js - Funções utilitárias
-const Utils = {
-    // Verifica colisão entre dois retângulos
-    colisao: function(a, b) {
-        return a.x < b.x + b.w && 
-               a.x + a.w > b.x && 
-               a.y < b.y + b.h && 
-               a.y + a.h > b.y;
-    },
-    
-    // Cria partículas de efeito
-    criarParticulas: function(ctx, x, y, quantidade, cor) {
-        for (let i = 0; i < quantidade; i++) {
-            const particula = {
-                x: x + Math.random() * 30 - 15,
-                y: y + Math.random() * 30 - 15,
-                vx: Math.random() * 6 - 3,
-                vy: Math.random() * -4 - 2,
-                tamanho: Math.random() * 4 + 2,
-                vida: 25,
-                cor: cor
-            };
-            
-            ctx.fillStyle = particula.cor;
-            ctx.beginPath();
-            ctx.arc(particula.x, particula.y, particula.tamanho, 0, Math.PI * 2);
-            ctx.fill();
-        }
-    },
-    
-    // Formata número para 2 dígitos
-    formatarNumero: function(num) {
-        return num < 10 ? '0' + num : num;
-    },
-    
-    // Desenha texto com opções
-    desenharTexto: function(ctx, texto, x, y, tamanho = 16, cor = "white", alinhamento = "left") {
-        ctx.fillStyle = cor;
-        ctx.font = `${tamanho}px Arial`;
-        ctx.textAlign = alinhamento;
-        ctx.fillText(texto, x, y);
-    }
-};
+// Constantes globais
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
-window.Utils = Utils;
+const GRAVIDADE = 0.8;
+const CHAO = 320;
+const LIM_ESQ = 40;
+const LIM_DIR = canvas.width - 40;
+
+// Variáveis globais
+let jogoTerminou = false;
+const keys = {};
+
+// Carrega imagem de fundo
+const fundo = new Image();
+fundo.src = "imagens/fundo.png";
+
+// Sistema de input
+addEventListener("keydown", e => keys[e.key] = true);
+addEventListener("keyup", e => keys[e.key] = false);
+
+// Função de colisão
+function colisao(a, b) {
+    return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.y + a.h > b.y;
+}
