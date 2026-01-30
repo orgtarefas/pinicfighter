1409afd5a28e21f64162c3c082c51c866d622b89
orgtarefas <orgtarefas.site@gmail.com>
2026-01-30 13:27:35 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -15,15 +15,66 @@ const keys = {};
 const fundo = new Image();
 fundo.src = "imagens/fundo.png";
 
-// Sistema de input
-addEventListener("keydown", e => keys[e.key] = true);
-addEventListener("keyup", e => keys[e.key] = false);
+// Sistema de input - CORRIGIDO PARA MULTIPLAYER
+addEventListener("keydown", e => {
+    // Usar o código da tecla diretamente
+    keys[e.code] = true;
+    
+    // Também manter compatibilidade com sistema antigo
+    const teclaAntiga = converterTeclaParaAntigo(e.code);
+    if (teclaAntiga) {
+        keys[teclaAntiga] = true;
+    }
+});
+
+addEventListener("keyup", e => {
+    keys[e.code] = false;
+    
+    // Também manter compatibilidade com sistema antigo
+    const teclaAntiga = converterTeclaParaAntigo(e.code);
+    if (teclaAntiga) {
+        keys[teclaAntiga] = false;
+    }
+});
+
+// Função para converter teclas do novo formato para o antigo
+function converterTeclaParaAntigo(teclaCode) {
+    const mapaConversao = {
+        'KeyA': 'a',
+        'KeyD': 'd', 
+        'KeyW': 'w',
+        'KeyF': 'f',
+        'KeyC': 'c',
+        'KeyS': 's',
+        
+        'ArrowLeft': 'ArrowLeft',
+        'ArrowRight': 'ArrowRight',
+        'ArrowUp': 'ArrowUp',
+        'Enter': 'Enter',
+        'Period': '.',
+        'ArrowDown': 'ArrowDown',
+        
+        'KeyJ': 'j',
+        'KeyL': 'l',
+        'KeyI': 'i',
+        'KeyH': 'h',
+        'KeyN': 'n',
+        'KeyK': 'k',
+        
+        'Numpad4': 'Numpad4',
+        'Numpad6': 'Numpad6',
+        'Numpad8': 'Numpad8',
+        'Numpad0': 'Numpad0',
+        'NumpadDecimal': 'NumpadDecimal',
+        'Numpad5': 'Numpad5'
+    };
+    
+    return mapaConversao[teclaCode];
+}
 
 // Função de colisão
 function colisao(a, b) {
     return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.y + a.h > b.y;
 }
 
-// REMOVIDO: const p1 = new LutadorCoco(...)
-// REMOVIDO: const p2 = new LutadorCoco(...)
-// Agora p1 e p2 serão declarados apenas em game.js
+console.log('✓ Utils.js carregado com suporte a multiplayer');
